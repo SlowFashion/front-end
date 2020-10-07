86b5fe21f78e0fc9902ac2f8406db95a8d43f647
Kim Kiamco <kim.kiamco@menlosecurity.com>
2020-10-06 20:16:49 -0700

    added item dexscription

--- src/components/itemDescription.js
+++ src/components/itemDescription.js
@@ -1,25 +1,113 @@
 import React from 'react';
-import {makeStyles} from '@material-ui/core/styles';
-import {Box} from '@material-ui/core';
-import { data } from '../data/testData1';
+import { makeStyles } from '@material-ui/core/styles';
+import { Box, Typography, Divider, Avatar, Button } from '@material-ui/core';
+import {primaryBtn} from '../components/themes/components';
+import Parser from 'html-react-parser';
 
 const useStyles = makeStyles({
-    root:{
+    root: {
+        display: 'flex',
+        flexFlow: 'row no wrap',
+        alignItems: 'flex-start'
+    },
+    poster: {
+        display: 'flex',
+        flexFlow: 'row wrap',
+        alignItems: 'center'
+    },
+    images: {
+        width: '50% !important',
+        height: '100vh !important',
+        backgroundPosition: 'center !important',
+        backgroundSize: 'cover !important'
+        //     top: "50%",
+        // height:'90vh',
+        // position: 'relative',
+        // transform: 'translateY(-50%)',
 
+    },
+    icons: {
+
+    },
+    description: {
+        margin:'20px 40px',
+        textAlign:'left',
+        maxWidth:'40vw'
+    },
+    detailsContainer: {
+        // margin: '20px',
+        // width: '50vw',
+        // display: 'flex',
+        // flexDirection: 'row',
+        // justifyContent: 'space-between',
+        // alignItems: 'center'
+    },
+    divders: {
+        marginTop: '20px',
+        width: '50vw'
+    },
+    posterDetails: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        justifyContent: 'space-around'
+    },
+    posterImg: {
+        marginRight: '10px',
+        height: '75px',
+        minWidth: '75px'
+    },
+    posterContainer:{
+        display:'flex',
+        flexFlow:'row wrap',
+        justifyContent:'space-between',
+        margin:'20px',
+        alignItems:'center'
+    },
+    cartBtn:{
+        ...primaryBtn,
+        height:'50px'
     }
+
+
 });
 
 
- const ItemDescription = ({name}) => {
-    
+const ItemDescription = ({ name, category, image, price, madeBy, location, rating, numReviews, description }) => {
+    console.log(image)
     const classes = useStyles();
 
-     return(
-        <Box>
-            <h1>{name}</h1>
+    return (
+        <Box className={classes.root}>
+
+            <Box style={{ background: `url(${image})` }} className={classes.images}>
+                {/* <img className={classes.images}  src={image} alt="" /> */}
+            </Box>
+
+            <Box className={classes.detailsContainer}>
+                <Box className={classes.posterContainer}>
+                    <Box className={classes.poster}>
+                        <Avatar className={classes.posterImg} />
+                        <Box className={classes.posterDetails}>
+                            <Typography style={{ lineHeight: 1 }} variant='h6'>{madeBy}</Typography>
+                            <Typography style={{ color: 'gray' }} variant='subtitle2'>{location}</Typography>
+                        </Box>
+                    </Box>
+
+                    <Button className={classes.cartBtn}>Add To Cart</Button>
+                </Box>
+                <Divider className={classes.divders} />
+                <Box className={classes.description}>
+                    <Typography> {Parser(description)} </Typography>
+                </Box>
+            </Box>
+
+
         </Box>
-     );
 
- };
 
- export default ItemDescription;
+    );
+
+};
+
+export default ItemDescription;
--- src/data/testData1.js
+++ src/data/testData1.js
@@ -1,5 +1,5 @@
 import pic1 from './img/abby-savage-PiKX8qPBFgU-unsplash.jpg';
-import pic2 from './img/ussama-azam-xgNSlx7DjYM-unsplash (1).jpg';
+import pic2 from './img/ussama-azam-xgNSlx7DjYM-unsplash.jpg';
 import pic3 from './img/tyler-nix-VpR8ZwcogAw-unsplash.jpg';
 
 
@@ -12,7 +12,29 @@ export const data = [
         category:'Shirts',
         image:pic1,
         price:15.99,
+        description:`<p class="Text-yok90d-0 styles__DescriptionContainer-uwktmu-9 bWcgji">💧Tommy Hilfiger quarter zip knit with buttons too - embroidered logo on cuff 
+        <br><br>
+        🎾colour: grey
+        <br><br>
+        🌿condition: good
+        <br><br>
+        📐measurements:<br>
+        • 63 cm length (shoulder to bottom)<br>
+        • 71 cm sleeve (shoulder to cuff)<br>
+        • 57 cm pit to pit <br>
+        <br><br>
+        ♻️ this item has been steamed however it is vintage so please expect any signs of wear, minor marks, vintage smells and bobbling 
+        (anything major will be mentioned or pictured)
+        <br><br>
+        📮£5 UK postage (instant buy is on in uk)<br>
+        🌏 message for international shipping<br>
+        📦 message for bundle deals<br>
+        <br><br>
+        🥶instagram: <a href="/iceicevintage" class="Link-sc-1urid-0 cuBKKA">@iceicevintage</a> 
+        <br>
+        <a href="/search/?q=%23tommyhilfiger" data-testid="product__hashtag--#tommyhilfiger" class="Link-sc-1urid-0 cuBKKA">#tommyhilfiger </a> <a href="/search/?q=%23jumper" data-testid="product__hashtag--#jumper" class="Link-sc-1urid-0 cuBKKA">#jumper </a> <a href="/search/?q=%23sweater" data-testid="product__hashtag--#sweater" class="Link-sc-1urid-0 cuBKKA">#sweater </a> <a href="/search/?q=%23knitted" data-testid="product__hashtag--#knitted" class="Link-sc-1urid-0 cuBKKA">#knitted </a> <a href="/search/?q=%23wool" data-testid="product__hashtag--#wool" class="Link-sc-1urid-0 cuBKKA">#wool </a></p>`,
         madeBy:'kim',
+        location:'United State, California',
         rating:5,
         numReviews:10
            
@@ -24,16 +46,28 @@ export const data = [
         image:pic1,
         price:20,
         madeBy:'kim',
-        rating:3,
-        numReviews:10   
-    },
-    {
-        _id:3,
-        name:'Cardigan',
-        category:'sweater',
-        image: pic1,
-        price:15.99,
-        madeBy:'kim',
+        description:`<p class="Text-yok90d-0 styles__DescriptionContainer-uwktmu-9 bWcgji">💧Tommy Hilfiger quarter zip knit with buttons too - embroidered logo on cuff 
+        <br><br>
+        🎾colour: grey
+        <br><br>
+        🌿condition: good
+        <br><br>
+        📐measurements:<br>
+        • 63 cm length (shoulder to bottom)<br>
+        • 71 cm sleeve (shoulder to cuff)<br>
+        • 57 cm pit to pit <br>
+        <br><br>
+        ♻️ this item has been steamed however it is vintage so please expect any signs of wear, minor marks, vintage smells and bobbling 
+        (anything major will be mentioned or pictured)
+        <br><br>
+        📮£5 UK postage (instant buy is on in uk)<br>
+        🌏 message for international shipping<br>
+        📦 message for bundle deals<br>
+        <br><br>
+        🥶instagram: <a href="/iceicevintage" class="Link-sc-1urid-0 cuBKKA">@iceicevintage</a> 
+        <br>
+        <a href="/search/?q=%23tommyhilfiger" data-testid="product__hashtag--#tommyhilfiger" class="Link-sc-1urid-0 cuBKKA">#tommyhilfiger </a> <a href="/search/?q=%23jumper" data-testid="product__hashtag--#jumper" class="Link-sc-1urid-0 cuBKKA">#jumper </a> <a href="/search/?q=%23sweater" data-testid="product__hashtag--#sweater" class="Link-sc-1urid-0 cuBKKA">#sweater </a> <a href="/search/?q=%23knitted" data-testid="product__hashtag--#knitted" class="Link-sc-1urid-0 cuBKKA">#knitted </a> <a href="/search/?q=%23wool" data-testid="product__hashtag--#wool" class="Link-sc-1urid-0 cuBKKA">#wool </a></p>`,
+        location:'United State, California',
         rating:4.5,
         numReviews:0   
     },
@@ -44,6 +78,28 @@ export const data = [
         image:pic2,
         price:15.99,
         madeBy:'kim',
+        description:`<p class="Text-yok90d-0 styles__DescriptionContainer-uwktmu-9 bWcgji">💧Tommy Hilfiger quarter zip knit with buttons too - embroidered logo on cuff 
+        <br><br>
+        🎾colour: grey
+        <br><br>
+        🌿condition: good
+        <br><br>
+        📐measurements:<br>
+        • 63 cm length (shoulder to bottom)<br>
+        • 71 cm sleeve (shoulder to cuff)<br>
+        • 57 cm pit to pit <br>
+        <br><br>
+        ♻️ this item has been steamed however it is vintage so please expect any signs of wear, minor marks, vintage smells and bobbling 
+        (anything major will be mentioned or pictured)
+        <br><br>
+        📮£5 UK postage (instant buy is on in uk)<br>
+        🌏 message for international shipping<br>
+        📦 message for bundle deals<br>
+        <br><br>
+        🥶instagram: <a href="/iceicevintage" class="Link-sc-1urid-0 cuBKKA">@iceicevintage</a> 
+        <br>
+        <a href="/search/?q=%23tommyhilfiger" data-testid="product__hashtag--#tommyhilfiger" class="Link-sc-1urid-0 cuBKKA">#tommyhilfiger </a> <a href="/search/?q=%23jumper" data-testid="product__hashtag--#jumper" class="Link-sc-1urid-0 cuBKKA">#jumper </a> <a href="/search/?q=%23sweater" data-testid="product__hashtag--#sweater" class="Link-sc-1urid-0 cuBKKA">#sweater </a> <a href="/search/?q=%23knitted" data-testid="product__hashtag--#knitted" class="Link-sc-1urid-0 cuBKKA">#knitted </a> <a href="/search/?q=%23wool" data-testid="product__hashtag--#wool" class="Link-sc-1urid-0 cuBKKA">#wool </a></p>`,
+        location:'United State, California',
         rating:5,
         numReviews:10   
     },
@@ -54,6 +110,28 @@ export const data = [
         image:pic3,
         price:15.99,
         madeBy:'kim',
+        description:`<p class="Text-yok90d-0 styles__DescriptionContainer-uwktmu-9 bWcgji">💧Tommy Hilfiger quarter zip knit with buttons too - embroidered logo on cuff 
+        <br><br>
+        🎾colour: grey
+        <br><br>
+        🌿condition: good
+        <br><br>
+        📐measurements:<br>
+        • 63 cm length (shoulder to bottom)<br>
+        • 71 cm sleeve (shoulder to cuff)<br>
+        • 57 cm pit to pit <br>
+        <br><br>
+        ♻️ this item has been steamed however it is vintage so please expect any signs of wear, minor marks, vintage smells and bobbling 
+        (anything major will be mentioned or pictured)
+        <br><br>
+        📮£5 UK postage (instant buy is on in uk)<br>
+        🌏 message for international shipping<br>
+        📦 message for bundle deals<br>
+        <br><br>
+        🥶instagram: <a href="/iceicevintage" class="Link-sc-1urid-0 cuBKKA">@iceicevintage</a> 
+        <br>
+        <a href="/search/?q=%23tommyhilfiger" data-testid="product__hashtag--#tommyhilfiger" class="Link-sc-1urid-0 cuBKKA">#tommyhilfiger </a> <a href="/search/?q=%23jumper" data-testid="product__hashtag--#jumper" class="Link-sc-1urid-0 cuBKKA">#jumper </a> <a href="/search/?q=%23sweater" data-testid="product__hashtag--#sweater" class="Link-sc-1urid-0 cuBKKA">#sweater </a> <a href="/search/?q=%23knitted" data-testid="product__hashtag--#knitted" class="Link-sc-1urid-0 cuBKKA">#knitted </a> <a href="/search/?q=%23wool" data-testid="product__hashtag--#wool" class="Link-sc-1urid-0 cuBKKA">#wool </a></p>`,
+        location:'United State, California',
         rating:5,
         numReviews:10   
     },
@@ -64,6 +142,28 @@ export const data = [
         image:pic1,
         price:15.99,
         madeBy:'kim',
+        description:`<p class="Text-yok90d-0 styles__DescriptionContainer-uwktmu-9 bWcgji">💧Tommy Hilfiger quarter zip knit with buttons too - embroidered logo on cuff 
+        <br><br>
+        🎾colour: grey
+        <br><br>
+        🌿condition: good
+        <br><br>
+        📐measurements:<br>
+        • 63 cm length (shoulder to bottom)<br>
+        • 71 cm sleeve (shoulder to cuff)<br>
+        • 57 cm pit to pit <br>
+        <br><br>
+        ♻️ this item has been steamed however it is vintage so please expect any signs of wear, minor marks, vintage smells and bobbling 
+        (anything major will be mentioned or pictured)
+        <br><br>
+        📮£5 UK postage (instant buy is on in uk)<br>
+        🌏 message for international shipping<br>
+        📦 message for bundle deals<br>
+        <br><br>
+        🥶instagram: <a href="/iceicevintage" class="Link-sc-1urid-0 cuBKKA">@iceicevintage</a> 
+        <br>
+        <a href="/search/?q=%23tommyhilfiger" data-testid="product__hashtag--#tommyhilfiger" class="Link-sc-1urid-0 cuBKKA">#tommyhilfiger </a> <a href="/search/?q=%23jumper" data-testid="product__hashtag--#jumper" class="Link-sc-1urid-0 cuBKKA">#jumper </a> <a href="/search/?q=%23sweater" data-testid="product__hashtag--#sweater" class="Link-sc-1urid-0 cuBKKA">#sweater </a> <a href="/search/?q=%23knitted" data-testid="product__hashtag--#knitted" class="Link-sc-1urid-0 cuBKKA">#knitted </a> <a href="/search/?q=%23wool" data-testid="product__hashtag--#wool" class="Link-sc-1urid-0 cuBKKA">#wool </a></p>`,
+        location:'United State, California',
         rating:5,
         numReviews:10   
     },
@@ -74,6 +174,28 @@ export const data = [
         image:pic1,
         price:20,
         madeBy:'kim',
+        description:`<p class="Text-yok90d-0 styles__DescriptionContainer-uwktmu-9 bWcgji">💧Tommy Hilfiger quarter zip knit with buttons too - embroidered logo on cuff 
+        <br><br>
+        🎾colour: grey
+        <br><br>
+        🌿condition: good
+        <br><br>
+        📐measurements:<br>
+        • 63 cm length (shoulder to bottom)<br>
+        • 71 cm sleeve (shoulder to cuff)<br>
+        • 57 cm pit to pit <br>
+        <br><br>
+        ♻️ this item has been steamed however it is vintage so please expect any signs of wear, minor marks, vintage smells and bobbling 
+        (anything major will be mentioned or pictured)
+        <br><br>
+        📮£5 UK postage (instant buy is on in uk)<br>
+        🌏 message for international shipping<br>
+        📦 message for bundle deals<br>
+        <br><br>
+        🥶instagram: <a href="/iceicevintage" class="Link-sc-1urid-0 cuBKKA">@iceicevintage</a> 
+        <br>
+        <a href="/search/?q=%23tommyhilfiger" data-testid="product__hashtag--#tommyhilfiger" class="Link-sc-1urid-0 cuBKKA">#tommyhilfiger </a> <a href="/search/?q=%23jumper" data-testid="product__hashtag--#jumper" class="Link-sc-1urid-0 cuBKKA">#jumper </a> <a href="/search/?q=%23sweater" data-testid="product__hashtag--#sweater" class="Link-sc-1urid-0 cuBKKA">#sweater </a> <a href="/search/?q=%23knitted" data-testid="product__hashtag--#knitted" class="Link-sc-1urid-0 cuBKKA">#knitted </a> <a href="/search/?q=%23wool" data-testid="product__hashtag--#wool" class="Link-sc-1urid-0 cuBKKA">#wool </a></p>`,
+        location:'United State, California',
         rating:3,
         numReviews:10   
     },
@@ -84,6 +206,28 @@ export const data = [
         image: pic1,
         price:15.99,
         madeBy:'kim',
+        description:`<p class="Text-yok90d-0 styles__DescriptionContainer-uwktmu-9 bWcgji">💧Tommy Hilfiger quarter zip knit with buttons too - embroidered logo on cuff 
+        <br><br>
+        🎾colour: grey
+        <br><br>
+        🌿condition: good
+        <br><br>
+        📐measurements:<br>
+        • 63 cm length (shoulder to bottom)<br>
+        • 71 cm sleeve (shoulder to cuff)<br>
+        • 57 cm pit to pit <br>
+        <br><br>
+        ♻️ this item has been steamed however it is vintage so please expect any signs of wear, minor marks, vintage smells and bobbling 
+        (anything major will be mentioned or pictured)
+        <br><br>
+        📮£5 UK postage (instant buy is on in uk)<br>
+        🌏 message for international shipping<br>
+        📦 message for bundle deals<br>
+        <br><br>
+        🥶instagram: <a href="/iceicevintage" class="Link-sc-1urid-0 cuBKKA">@iceicevintage</a> 
+        <br>
+        <a href="/search/?q=%23tommyhilfiger" data-testid="product__hashtag--#tommyhilfiger" class="Link-sc-1urid-0 cuBKKA">#tommyhilfiger </a> <a href="/search/?q=%23jumper" data-testid="product__hashtag--#jumper" class="Link-sc-1urid-0 cuBKKA">#jumper </a> <a href="/search/?q=%23sweater" data-testid="product__hashtag--#sweater" class="Link-sc-1urid-0 cuBKKA">#sweater </a> <a href="/search/?q=%23knitted" data-testid="product__hashtag--#knitted" class="Link-sc-1urid-0 cuBKKA">#knitted </a> <a href="/search/?q=%23wool" data-testid="product__hashtag--#wool" class="Link-sc-1urid-0 cuBKKA">#wool </a></p>`,
+        location:'United State, California',
         rating:4.5,
         numReviews:0   
     },
@@ -94,6 +238,28 @@ export const data = [
         image:pic2,
         price:15.99,
         madeBy:'kim',
+        description:`<p class="Text-yok90d-0 styles__DescriptionContainer-uwktmu-9 bWcgji">💧Tommy Hilfiger quarter zip knit with buttons too - embroidered logo on cuff 
+        <br><br>
+        🎾colour: grey
+        <br><br>
+        🌿condition: good
+        <br><br>
+        📐measurements:<br>
+        • 63 cm length (shoulder to bottom)<br>
+        • 71 cm sleeve (shoulder to cuff)<br>
+        • 57 cm pit to pit <br>
+        <br><br>
+        ♻️ this item has been steamed however it is vintage so please expect any signs of wear, minor marks, vintage smells and bobbling 
+        (anything major will be mentioned or pictured)
+        <br><br>
+        📮£5 UK postage (instant buy is on in uk)<br>
+        🌏 message for international shipping<br>
+        📦 message for bundle deals<br>
+        <br><br>
+        🥶instagram: <a href="/iceicevintage" class="Link-sc-1urid-0 cuBKKA">@iceicevintage</a> 
+        <br>
+        <a href="/search/?q=%23tommyhilfiger" data-testid="product__hashtag--#tommyhilfiger" class="Link-sc-1urid-0 cuBKKA">#tommyhilfiger </a> <a href="/search/?q=%23jumper" data-testid="product__hashtag--#jumper" class="Link-sc-1urid-0 cuBKKA">#jumper </a> <a href="/search/?q=%23sweater" data-testid="product__hashtag--#sweater" class="Link-sc-1urid-0 cuBKKA">#sweater </a> <a href="/search/?q=%23knitted" data-testid="product__hashtag--#knitted" class="Link-sc-1urid-0 cuBKKA">#knitted </a> <a href="/search/?q=%23wool" data-testid="product__hashtag--#wool" class="Link-sc-1urid-0 cuBKKA">#wool </a></p>`,
+        location:'United State, California',
         rating:5,
         numReviews:10   
     },
@@ -104,6 +270,28 @@ export const data = [
         image:pic3,
         price:15.99,
         madeBy:'kim',
+        description:`<p class="Text-yok90d-0 styles__DescriptionContainer-uwktmu-9 bWcgji">💧Tommy Hilfiger quarter zip knit with buttons too - embroidered logo on cuff 
+        <br><br>
+        🎾colour: grey
+        <br><br>
+        🌿condition: good
+        <br><br>
+        📐measurements:<br>
+        • 63 cm length (shoulder to bottom)<br>
+        • 71 cm sleeve (shoulder to cuff)<br>
+        • 57 cm pit to pit <br>
+        <br><br>
+        ♻️ this item has been steamed however it is vintage so please expect any signs of wear, minor marks, vintage smells and bobbling 
+        (anything major will be mentioned or pictured)
+        <br><br>
+        📮£5 UK postage (instant buy is on in uk)<br>
+        🌏 message for international shipping<br>
+        📦 message for bundle deals<br>
+        <br><br>
+        🥶instagram: <a href="/iceicevintage" class="Link-sc-1urid-0 cuBKKA">@iceicevintage</a> 
+        <br>
+        <a href="/search/?q=%23tommyhilfiger" data-testid="product__hashtag--#tommyhilfiger" class="Link-sc-1urid-0 cuBKKA">#tommyhilfiger </a> <a href="/search/?q=%23jumper" data-testid="product__hashtag--#jumper" class="Link-sc-1urid-0 cuBKKA">#jumper </a> <a href="/search/?q=%23sweater" data-testid="product__hashtag--#sweater" class="Link-sc-1urid-0 cuBKKA">#sweater </a> <a href="/search/?q=%23knitted" data-testid="product__hashtag--#knitted" class="Link-sc-1urid-0 cuBKKA">#knitted </a> <a href="/search/?q=%23wool" data-testid="product__hashtag--#wool" class="Link-sc-1urid-0 cuBKKA">#wool </a></p>`,
+        location:'United State, California',
         rating:5,
         numReviews:10   
     }
